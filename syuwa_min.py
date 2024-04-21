dded3acf1f414ac34f8b2658e72dc7e12539e11b
Ryo Murakami <ryoyuki6@yahoo.co.jp>
2024-04-21 17:43:15 +0900

    update price

--- syuwa_min.py
+++ syuwa_min.py
@@ -18,7 +18,7 @@ if __name__=="__main__":
 
         result = dict()
         result["title"] = driver.find_element(By.CLASS_NAME, "titleWrap").text
-        result["price"] = driver.find_element(By.XPATH, '//*[@id="main"]/div[3]/div[2]/table/tbody/tr[6]/td').text
+        result["price"] = driver.find_element(By.XPATH, '//*[@id="main"]/div[3]/div[2]/table/tbody/tr[7]/td').text
         result["author"] = driver.find_element(By.CSS_SELECTOR, "#main > div.detail > div.right > table > tbody > tr:nth-child(1) > td > a").text
         result["describe"] = driver.find_element(By.ID, "bookSample").text
         print(result)
